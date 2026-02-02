34dca9e53110145b98b004848d35a44aaef2ba17
lewyinn <kurniawanridho500@gmail.com>
2026-02-02 07:28:22 +0700

    done for add lightBoxImage

--- src/components/home/MovieCard.js
+++ src/components/home/MovieCard.js
@@ -32,13 +32,23 @@ export default function MovieCard({ movie, compact = false }) {
                         </span>
                     </div>
 
-                    {movie.status === 'watching' && movie.lastWatchedMinute && (
+                    {movie.status === 'watching' && (
                         <div className="mb-2">
                             <div className="flex justify-between text-xs text-gray-600 mb-1">
                                 <span>Progress</span>
-                                <span>{getProgress()}%</span>
+                                {/* <span>{getProgress()}%</span> */}
                             </div>
-                            <div className="w-full bg-gray-200 rounded-full h-2">
+                            <div className="">
+                                {/* Episode dan Season */}
+                                <p className="text-xs text-gray-500 mt-1">
+                                    {movie.currentEpisode} Episode & Season {movie.currentSeason}
+                                </p>
+                                
+                            </div>
+                            <div className="mt-3 bg-gray-50 p-2 rounded-lg border border-gray-100">
+                                <p className="text-xs text-gray-500 italic line-clamp-2">"{movie.note}"</p>
+                            </div>
+                            {/* <div className="w-full bg-gray-200 rounded-full h-2">
                                 <div
                                     className="bg-gradient-to-r from-[#28C3B0] to-[#28c357] h-2 rounded-full transition-all"
                                     style={{ width: `${getProgress()}%` }}
@@ -46,7 +56,7 @@ export default function MovieCard({ movie, compact = false }) {
                             </div>
                             <div className="text-xs text-gray-500 mt-1">
                                 {movie.lastWatchedMinute} / {movie.totalMinutes} minutes
-                            </div>
+                            </div> */}
                         </div>
                     )}
 
@@ -56,8 +66,7 @@ export default function MovieCard({ movie, compact = false }) {
                                 <svg
                                     key={i}
                                     className={`w-4 h-4 ${i < movie.rating ? 'fill-current' : 'fill-gray-300'}`}
-                                    viewBox="0 0 20 20"
-                                >
+                                    viewBox="0 0 20 20">
                                     <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                                 </svg>
                             ))}
